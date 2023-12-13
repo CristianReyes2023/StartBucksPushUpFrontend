b3fc57c6189b90753b3b21783ab99cdc7fa0e588
CristianReyes2023 <137292974+CristianReyes2023@users.noreply.github.com>
2023-12-13 13:13:50 -0500

    feat: :sparkles: Mobile FirstFinished

--- js/main.js
+++ js/main.js
@@ -11,5 +11,32 @@ close.addEventListener('click', () => {
 });
 
 function imgSlider(anything){
-  document.querySelector('.starbucks').src = anything;
+  document.getElementById('sliderimage').src = anything;
 }
+let img = document.getElementById("sliderimage");
+let ba = document.getElementById("bebidaA");
+let bb = document.getElementById("bebidaB");
+let bc = document.getElementById("bebidaC");
+ba.addEventListener("click", function(){
+    img.src = "images/img1.png";
+})
+bb.addEventListener("click", function(){
+    img.src = "images/img2.png";
+})
+bc.addEventListener("click", function(){
+    img.src = "images/img3.png";
+}) 
+
+let circle = document.getElementById("circle");
+let colora = document.getElementById("bebidaA");
+let colorb = document.getElementById("bebidaB");
+let colorc = document.getElementById("bebidaC");
+colora.addEventListener("click", function(){
+    circle.style.background = "#2c4e2c";
+})
+colorb.addEventListener("click", function(){
+  circle.style.background = "#ec7a99";
+})
+colorc.addEventListener("click", function(){
+  circle.style.background = "#aa047a";
+})
